7ec3edaffb35dcf781fa20cb9b97d12e37953805
digitpoi <hubo85@gmail.com>
2017-07-25 18:20:47 -0500

    Add minute hand movement

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
+    //  second hand
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+
+    //  minute hand
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    
 }
 
 setInterval(setDate, 1000);
