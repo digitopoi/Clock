a76cb5d5870069b1c6ebc82562514a9acab4247f
digitpoi <hubo85@gmail.com>
2017-07-25 18:25:40 -0500

    Add hour hand functionality

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ function setDate() {
     //  minute hand
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
-    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     
+    
+    //  hours hand
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
